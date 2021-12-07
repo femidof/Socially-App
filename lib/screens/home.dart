af488f0a0fd4930b905196ff3d26af3c01a494e7
Cold Code <dee_femifose@yahoo.com>
2021-12-07 09:22:16 -0600

    permission camera misbehaving so i commented out

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:socially/screens/home_launch_screen.dart';
 import 'package:super_easy_permissions/super_easy_permissions.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -14,11 +17,14 @@ class HomeScreen extends StatelessWidget {
   }
 
   Future<bool> askForPermission() async {
-    if (await SuperEasyPermissions.askPermission(Permissions.camera) &&
-        await SuperEasyPermissions.askPermission(Permissions.contacts)) {
-      return true;
-    }
-    return false;
+    // if (
+    // await SuperEasyPermissions.askPermission(Permissions.camera
+    // ) &&
+    return await SuperEasyPermissions.askPermission(Permissions.contacts);
+    //  ) {
+    // return true;
+    // }
+    // return false;
   }
 
   @override
@@ -40,8 +46,11 @@ class HomeScreen extends StatelessWidget {
                       body: Center(
                         child: GestureDetector(
                             onTap: () async {
-                              print("ask");
-                              askForPermission();
+                              SystemChannels.textInput
+                                  .invokeMethod('TextInput.hide');
+                              print("askPls");
+                              await askForPermission();
+                              await mainPermissionasker();
                             },
                             child: Text("Failed to accept permission")),
                       ),
@@ -94,5 +103,28 @@ class HomeScreen extends StatelessWidget {
     //     ],
     //   )),
     // );
+  }
+
+  mainPermissionasker() async {
+    var status = Permission.contacts;
+    if (await status.isDenied) {
+      // We didn't ask for permission yet or the permission has been denied before but not permanently.
+      print("Rejected Please try again");
+      openAppSettings();
+      await Permission.contacts.request();
+    }
+    await Permission.contacts.request();
+    if (await Permission.contacts.request().isGranted) {
+      // Either the permission was already granted before or the user just granted it.
+      // Get.to();
+      print("Accepted Thank you");
+    }
+    if (await Permission.contacts.isPermanentlyDenied) {
+      // The user opted to never again see the permission request dialog for this
+      // app. The only way to change the permission's status now is to let the
+      // user manually enable it in the system settings.
+      print("Next level");
+      openAppSettings();
+    }
   }
 }
